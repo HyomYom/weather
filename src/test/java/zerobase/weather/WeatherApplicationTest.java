81039a1c63feddd932e3878ac50344a5f32c8ca0
HyomYom <hyomyang@gmail.com>
2023-07-03 09:50:24 +0900

    first

---
+++ src/test/java/zerobase/weather/WeatherApplicationTest.java
@@ -0,0 +1,16 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class WeatherApplicationTest {
+    @Test
+        void equalTest () {
+            //given
+        assertEquals(1, 1);
+            //when
+            //then
+        }
+}
